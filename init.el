d9f05e01f37f6d99a93841c62768ef2e27b53583
YulongNiu <niuylscu@gmail.com>
2015-09-09 17:29:43 +0800

    modify init.el

--- init.el
+++ init.el
@@ -58,7 +58,23 @@
 ;;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'company-backends 'company-c-headers)
 
+;; (eval-after-load 'company
+;;   '(add-to-list 'company-backends 'company-irony))
 
+;; (require 'irony)
+;; (add-hook 'c++-mode-hook 'irony-mode)
+;; (add-hook 'c-mode-hook 'irony-mode)
+;; (add-hook 'objc-mode-hook 'irony-mode)
+
+;; ;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; ;; irony-mode's buffers by irony-mode's function
+;; (defun my-irony-mode-hook ()
+;;   (define-key irony-mode-map [remap completion-at-point]
+;;     'irony-completion-at-point-async)
+;;   (define-key irony-mode-map [remap complete-symbol]
+;;     'irony-completion-at-point-async))
+;; (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 
 ;;;;;;;;;;;;;;;;;;;
 ;; function-args ;;
